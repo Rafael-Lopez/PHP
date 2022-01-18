5864d517b8739c89f94f6c84a5d60d43bb937b0d
Rafael Lopez <lopez.rafael08@gmail.com>
2022-01-18 01:43:37 -0500

    1_Basics: Comments in PHP

--- 1_Basics/index.php
+++ 1_Basics/index.php
@@ -1,9 +1,13 @@
 <?php 
+// Single line comment
     echo 'Hello World';
 ?>    
 
 <?='Hello World'?>
 
 <?php
+    /* We prefer to use echo
+       print is a little slower 
+    */
     print 'Hello World'
 ?>
