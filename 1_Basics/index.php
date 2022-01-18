64b7f15c9855819c947109dc3aeb44ad420bbdca
Rafael Lopez <lopez.rafael08@gmail.com>
2022-01-18 01:56:28 -0500

    1_Basics: Variables & Constants

--- 1_Basics/index.php
+++ 1_Basics/index.php
@@ -11,3 +11,19 @@
     */
     print 'Hello World'
 ?>
+
+<?php
+    $age = 34;
+    $hello_message = "Hello there!";
+    $just_a_boolean = true;
+    // Define a constant
+    define("PI", 3.1416);
+
+    // This will not work
+    // PI = 7;
+
+    echo $age;
+    echo $hello_message;
+    echo $just_a_boolean;
+    echo PI;
+?>
